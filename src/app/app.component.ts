a23777e4c96378c2d3702587ba8b91fd8ce338b8
Juan Cruz Alderete <juanx.alderete@tercerpiso.net>
2024-12-11 11:52:31 -0300

    Add filtros a las tablas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedImports } from './shared-imports';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedImports],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'Angular18Project';
+  products: any[] = [
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'OUTSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'OUTSTOCK',
+      rating: 5,
+    },
+  ];
+
+  inventoryStatusOptions: any[] = [
+    { label: 'All', value: null },
+    { label: 'In Stock', value: 'INSTOCK' },
+    { label: 'Out of Stock', value: 'OUTSTOCK' },
+  ];
+
+  selectedInventoryStatus: string | null = null;
+
+  filteredProducts: any[] = [...this.products];
+
+  searchValue: string = '';
+
+  contructor() {}
+
+  filterTable(): void {
+    if (this.selectedInventoryStatus) {
+      this.filteredProducts = this.products.filter(
+        (product) => product.inventoryStatus === this.selectedInventoryStatus
+      );
+    } else {
+      this.filteredProducts = [...this.products];
+    }
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.searchValue = '';
+    this.selectedInventoryStatus = null;
+    this.filteredProducts = [...this.products];
+  }
 }
---
+++ src/app/shared-imports.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PaginatorModule } from 'primeng/paginator';
+import { TableModule } from 'primeng/table';
+import { Select } from 'primeng/select';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+
+export const SharedImports = [
+  CommonModule,
+  FormsModule,
+  TableModule,
+  PaginatorModule,
+  Select,
+  ButtonModule,
+  IconFieldModule,
+  InputIconModule,
+  InputTextModule
+];
